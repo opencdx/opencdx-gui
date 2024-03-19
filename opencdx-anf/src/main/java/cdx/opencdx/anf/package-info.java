d6c647632c378cca94bffb38aea054c2b6d64104
Jeffery Miller <jeff.miller@safehealth.me>
2024-03-18 23:09:26 -0400

    Removed Backend Code

--- opencdx-anf/src/main/java/cdx/opencdx/anf/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This microservice is responsible for persisting Alternate Normal Form (ANF) statements.
- * ANF is a method applied in structuring data held within HL7 v2 messages, aiming to enhance its applicability for data analysis. It predominantly involves the categorization of data elements into more manageable, logical segments, driven by their interrelationships and usage frequency. This practice facilitates an improved understanding and manipulation of data, as well as an elevated ease of analysis, as opposed to the flat, linear structure characteristic of traditional HL7 v2 messages.
- *
- * @author Safe Health LLC.
- * @version 1.0
- * @since 1.0
- */
-package cdx.opencdx.anf;
--- opencdx-questionnaire/src/main/java/cdx/opencdx/questionnaire/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * A service for providing Forms for questionnaires on specific topics, allowed for processing by
- * the classification system and subsequent systems based on the analysis of the results.
- *
- * @author Safe Health LLC.
- * @version 1.0
- * @since 1.0
- */
-package cdx.opencdx.questionnaire;
