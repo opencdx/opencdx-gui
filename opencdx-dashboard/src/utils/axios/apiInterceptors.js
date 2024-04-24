55ad0af0f2baefb293a71f939694f726e19704b4
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-04-24 12:22:57 -0400

    Add back apiInterceptors

---
+++ opencdx-dashboard/src/utils/axios/apiInterceptors.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const API_URL = 'https://ec2-3-13-148-183.us-east-2.compute.amazonaws.com:8080'; // <- ENV variable;
+const apiInterceptors = axios.create({
+    baseURL: API_URL, // <- ENV variable
+});
+apiInterceptors.interceptors.request.use((config) => {
+    return ({
+        ...config,
+        headers: {
+            ...config.headers,
+            Authorization: `Bearer ${localStorage.getItem('serviceToken')}` || '',
+        },
+    })
+},
+    error => Promise.reject(error),
+);
+
+apiInterceptors.interceptors.response.use((response) =>
+    response,
+    async (error) => {
+    return Promise.reject(error.response.data);
+  },
+);
+const { get, post, put, destroy } = apiInterceptors;
+export { get, post, put, destroy };
+
+export default apiInterceptors;
