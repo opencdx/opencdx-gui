2c54f8c0122976bd59c33c0d7efc48be5c0dcffb
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-07-10 17:28:28 +0530

    Refactor SubjectofRecord and CircumstanceChoice components

--- opencdx-dashboard/src/formBuilder/components/Tabs/CircumstanceChoice.js
+++ opencdx-dashboard/src/formBuilder/components/Tabs/CircumstanceChoice.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { MeasureComponent } from '../TabComponents/MeasureComponent';
-import { ParticipantComponent } from '../TabComponents/ParticipantComponent';
+// import { ParticipantComponent } from '../TabComponents/ParticipantComponent';
 import { FormControl, Grid, TextField } from '@mui/material';
 import { MainCard } from '../ui-component/MainCard';
 import { InputLabel } from '../ui-component/InputLabel';
@@ -14,7 +15,10 @@ import { useAnfFormStore } from '../../utils/useAnfFormStore';
 export const CircumstanceChoice = React.forwardRef(({ control, register, index, currentIndex, getValues }, ref) => {
     const { formData } = useAnfFormStore();
     const componentType = [statementType.MAIN, statementType.ASSOCIATED].includes(formData.item[index]?.componentType);
-
+    const [id, setId] = useState(systemVariables['circumstance']?.id);
+    const [practitioner, setPractitioner] = useState(systemVariables['circumstance']?.practitionerValue);
+    const [code, setCode] = useState(systemVariables['circumstance']?.code);
+  
     return (
         <Grid item xs={12} lg={12} ref={ref}>
             <SystemVariables index={index} currentIndex={currentIndex} getValues={getValues} tab="performanceCircumstance" />
@@ -85,7 +89,92 @@ export const CircumstanceChoice = React.forwardRef(({ control, register, index, 
                     <Grid item xs={12} sm={3} lg={4} sx={{ pt: { xs: 2, sm: '0 !important', marginTop: 10 } }}>
                         <InputLabel horizontal>Participant</InputLabel>
                     </Grid>
-                    {<ParticipantComponent {...{ control, register, index, currentIndex }} tab={'performanceCircumstance.participant'} />}
+                    <Grid container spacing={1} alignItems="center">
+                            <Grid item xs={12} sm={3} lg={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                                <InputLabel horizontal>ID</InputLabel>
+                            </Grid>
+                            <Grid item xs={12} sm={9} lg={8}>
+                                {componentType ? (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatement.performanceCircumstance.participant.id`)}
+                                        fullWidth
+                                        type="text"
+                                        value={id}
+                                        placeholder="Enter ID Value"
+                                        onChange={(e) => {
+                                            setId(e.target.value);
+                                        }}
+                                    />
+                                ) : (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatement.performanceCircumstance.participant.id`)}
+                                        fullWidth
+                                        type="text"
+                                        InputProps={{
+                                            inputProps: { min: 0 }
+                                        }}
+                                        placeholder="Enter ID Value"
+                                    />
+                                )}
+                            </Grid>
+                            <Grid item xs={12} sm={3} lg={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                                <InputLabel horizontal>practitioner</InputLabel>
+                            </Grid>
+                            <Grid item xs={12} sm={9} lg={8}>
+                                {componentType ? (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatement.performanceCircumstance.participant.practitionerValue`)}
+                                        fullWidth
+                                        type="text"
+                                        value={practitioner}
+                                        placeholder="Enter Practitioner Value"
+                                        onChange={(e) => {
+                                            setPractitioner(e.target.value);
+                                        }}
+                                    />
+                                ) : (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatementperformanceCircumstance.participant.practitionerValue`)}
+                                        fullWidth
+                                        type="text"
+                                        InputProps={{
+                                            inputProps: { min: 0 }
+                                        }}
+                                        placeholder="Enter Practitioner Value"
+                                    />
+                                )}
+                            </Grid>
+                            <Grid item xs={12} sm={3} lg={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                                <InputLabel horizontal>Code</InputLabel>
+                            </Grid>
+                            <Grid item xs={12} sm={9} lg={8}>
+                                {componentType ? (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatement.performanceCircumstance.participant.code`)}
+                                        fullWidth
+                                        type="text"
+                                        value={code}
+                                        placeholder="Enter Code Value"
+                                        onChange={(e) => {
+                                            setCode(e.target.value);
+                                        }}
+                                    />
+                                ) : (
+                                    <TextField
+                                        {...register(`item.${index}.anfStatementConnector.${currentIndex}.anfStatement.performanceCircumstance.participant.code`)}
+                                        fullWidth
+                                        type="text"
+                                        InputProps={{
+                                            inputProps: { min: 0 }
+                                        }}
+                                        placeholder="Enter Code Value"
+                                    />
+                                )}
+                            </Grid>
+                            
+                           
+                        </Grid>
+                    {/* {<ParticipantComponent {...{ control, register, index, currentIndex }} tab={'performanceCircumstance.participant'} />} */}
                 </Grid>
             </MainCard>
         </Grid>
--- opencdx-dashboard/src/formBuilder/components/Tabs/SubjectofRecord.js
+++ opencdx-dashboard/src/formBuilder/components/Tabs/SubjectofRecord.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import { SystemVariables } from '../SystemVariables';
 import { ParticipantComponent } from '../TabComponents/ParticipantComponent';
 
-const SubjectofRecord = React.forwardRef(({ register, index, getValues, currentIndex }, ref) => {
+const SubjectofRecord = React.forwardRef(({ control,register, index, getValues, currentIndex }, ref) => {
     return (
         <>
             <SystemVariables index={index} currentIndex={currentIndex} getValues={getValues} tab="subjectOfRecord" />
-            <ParticipantComponent {...{ register, index, currentIndex }} tab="subjectOfRecord" ref={ref} />
+            <ParticipantComponent {...{ register, index, currentIndex, control }} tab="subjectOfRecord" ref={ref} />
         </>
     );
 });
