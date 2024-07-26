4e38665dc8bcc43654dc361ebe14426fa864e5ab
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-07-26 20:13:26 +0530

    Remove unused API endpoints and environment variables

--- opencdx-dashboard-new/src/axios/apiEndpoints.js
+++ opencdx-dashboard-new/src/axios/apiEndpoints.js
@@ -1,8 +1,6 @@
 import { get, post, put, remove } from '@/axios/apiInterceptors';
 
 export const Endpoints = {
-    login: (params) => post('/iam/user/login', params),
-    signup: (params) => post('/iam/user/signup', params),
     userResponses: (params) => post('/questionnaire/questionnaire/list', params),
     getQuestionnaireList: (params) => post('/questionnaire/questionnaire/list', params),
     getQuestionnaireById: (params) => get('/questionnaire/questionnaire/' + params.id, params),
