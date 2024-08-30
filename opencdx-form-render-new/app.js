0a2be8444bd61c39cc71a405026e383ed70a0689
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-08-30 17:21:14 +0530

    Initial commit

---
+++ opencdx-form-render-new/app.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    // <View style={styles.container}>
+    <View className='flex-1 justify-center items-center bg-white'>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
