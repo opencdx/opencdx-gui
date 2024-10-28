142e9abb937f360b9ac21379d833e29ffcfe63b1
Kriti Goel <kriti@safehealth.me>
2024-10-29 01:08:33 +0530

    partially complete profile api's implementation

---
+++ opencdx-form-render/api/profile/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './UpdateProfile200Response';
+export * from './UpdateProfileRequest';
+export * from './UpdateProfileRequestUpdatedProfile';
+export * from './UpdateProfileRequestUpdatedProfileDateOfBirth';
+export * from './UpdateProfileRequestUpdatedProfileEmailInner';
+export * from './UpdateProfileRequestUpdatedProfileFullName';
+export * from './UpdateProfileRequestUpdatedProfilePhoneInner';
