1d3d23970d7e2d54822773c266dc98bb4188ad63
Kriti Goel <kriti@safehealth.me>
2024-11-11 19:07:25 +0530

    matched radio separator with figma

--- opencdx-form-render/components/ui/radioInput.tsx
+++ opencdx-form-render/components/ui/radioInput.tsx
@@ -24,24 +24,26 @@ export const RadioInput: React.FC<RadioInputProps> = ({
   return (
     <View className="p-4 w-full items-start">
       {label && <Text className="text-lg mb-4">{label}</Text>}
-      <RadioButton.Group onValueChange={onValueChange} value={value}>
-        {options.map((option, index) => (
-          <View key={index} className="w-full max-w-[800px] mx-auto">
-            {/* Top separator line for all option */}
-            <View className="h-px bg-gray-300 w-full" />
-            <View className="flex-row items-center py-4 w-full">
-              <RadioButton
-                value={option.value}
-                color="#0066cc" // Primary color for selected radio button
-                uncheckedColor="#E4E4E7"
-              />
-              <Text className="text-base font-normal">{option.label}</Text>
+      <View className="w-full">
+        <RadioButton.Group onValueChange={onValueChange} value={value}>
+            {options.map((option, index) => (
+            <View key={index} className="w-full max-w-[800px] mx-auto">
+                {/* Top separator line for all option */}
+                <View className="h-px bg-gray-300 w-full" />
+                <View className="flex-row items-center py-4 w-full">
+                <RadioButton
+                    value={option.value}
+                    color="#0066cc" // Primary color for selected radio button
+                    uncheckedColor="#E4E4E7"
+                />
+                <Text className="text-base font-normal">{option.label}</Text>
+                </View>
+                {/* Bottom separator line for just last option */}
+                {index === options.length - 1 && <View className="h-px bg-gray-300 w-full" />}
             </View>
-            {/* Bottom separator line for just last option */}
-            {index === options.length - 1 && <View className="h-px bg-gray-300 w-full" />}
-          </View>
-        ))}
-      </RadioButton.Group>
+            ))}
+        </RadioButton.Group>
+     </View>
     </View>
   );
 };
